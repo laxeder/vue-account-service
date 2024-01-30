bc543d9f95d06d5ea29108018f70beb1a8182991
Laxeder <laxederbr@gmail.com>
2024-01-29 23:58:25 -0300

    feat(header): Adicionando componente header globalmente

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+import HeaderComponent from './components/HeaderComponent.vue'
+
 import App from './App.vue'
 import router from './router'
 
@@ -10,5 +12,7 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+app.component('HeaderComponent', HeaderComponent)
 
 app.mount('#app')
