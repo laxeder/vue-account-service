a79a245bc346a4e9e9750787f532c2a89b4deb58
Laxeder <laxederbr@gmail.com>
2024-01-31 20:22:01 -0300

    feat(routes): Adicionando novas rotas

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,32 @@
 import { createRouter, createWebHistory } from 'vue-router'
+
 import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
+  routes: []
+})
+
+router.addRoute({
+  name: 'home',
+  path: '/',
+  component: HomeView
+})
+
+router.addRoute({
+  name: 'users',
+  path: '/users/:id',
+  component: import('@/views/UserView.vue'),
+  children: [{
+    path: 'create',
+    component: import('@/components/user/CreateUser.vue')
+  }]
+})
+
+router.addRoute({
+  name: 'not-found',
+  path: '/:pathMatch(.*)*',
+  component: import('@/views/NotFound.vue')
 })
 
 export default router
