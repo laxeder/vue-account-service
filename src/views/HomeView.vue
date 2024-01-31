40e25c5cb7ccd2bd882edb1cc2dbcbeca3099cce
Laxeder <laxederbr@gmail.com>
2024-01-30 23:34:10 -0300

    feat(home): Criando página home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,46 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <div style="align-items: center;">
+      <img class="logo" src="@/assets/logo.svg" />
+      <h1>O MELHOR!!</h1>
+      <h3>TRATAMENTO DE CONTA ESPECIALIZADO</h3>
+    </div>
+    <div class="content">
+      <p style="text-align: center;">Conteúdo em construção...</p>
+    </div>
   </main>
 </template>
+
+<style scoped>
+main {
+  padding: 16px;
+  width: 100%;
+  align-items: center;
+}
+
+h1 {
+  text-align: center;
+  font-size: 60px;
+  font-weight: 700;
+  color: hsla(160, 100%, 37%, 1);
+}
+
+h3 {
+  text-align: center;
+  font-size: large;
+  font-weight: 700;
+  color: var(--color-heading);
+}
+
+.content {
+  margin-top: 68px;
+}
+
+.logo {
+  width: 100%;
+  height: 170px;
+}
+</style>
