f502fb4736498092244041df51d84b4825588499
Laxeder <laxederbr@gmail.com>
2024-01-31 20:21:34 -0300

    feat(views): Adicionando rota de página não encontrada

---
+++ src/views/NotFound.vue
@@ -0,0 +1,22 @@
+<template>
+  <main>
+    <h1>Página não encontrada {{ $route.params.pathMatch }}</h1>
+  </main>
+</template>
+
+<style scoped>
+main {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+}
+
+h1 {
+  width: 100%;
+  text-align: center;
+  font-size: 60px;
+  font-weight: 700;
+  color: hsla(160, 100%, 37%, 1);
+}
+</style>
