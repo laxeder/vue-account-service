31e24cf4f379215c01bec8d5a9f694ae838bfa28
Laxeder <laxederbr@gmail.com>
2024-01-31 20:21:03 -0300

    feat(app): Adicionando componentes do usuário

---
+++ src/components/user/CreateUser.vue
@@ -0,0 +1,100 @@
+<script setup lang="ts">
+import router from '@/router'
+
+function onCreateUser() {
+  router.go(-1)
+}
+</script>
+
+<template>
+  <main>
+    <div class="content">
+      <p class="title">Criar usuário</p>
+      <div class="line"><br /></div>
+      <div class="form">
+        <div class="form-content">
+          <p>[Editar nome...]</p>
+          <div style="width: 100%; justify-content: flex-end; display: flex">
+            <button class="create-user-button" @click="onCreateUser">Criar usuário</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+main {
+  min-width: 100vw;
+  min-height: 100vh;
+  width: 100%;
+  height: 100%;
+  padding: 48px;
+  display: flex;
+  align-items: center;
+  justify-self: center;
+  background-color: rgb(0, 0, 0, 0.4);
+}
+
+.content {
+  min-width: 50%;
+  width: auto;
+  box-shadow: 0 4px 8px rgb(0, 0, 0, 1);
+  border: none;
+  border-radius: 8px;
+  background-color: var(--color-background);
+}
+
+.line {
+  height: 0.5rem;
+  background-color: var(--color-background);
+  box-shadow: 0px 4px 4px rgb(0, 0, 0, 0.7);
+  margin-bottom: 16px;
+}
+
+.form {
+  width: 100%;
+  height: auto;
+  padding: 16px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: none;
+  border-radius: 0px 0px 8px 8px;
+  background-color: var(--color-background);
+}
+
+.form-content {
+  width: 100%;
+  height: auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+
+.title {
+  width: 100%;
+  margin-top: 1.5rem;
+  margin-bottom: 1rem;
+  text-align: center;
+  font-size: 24px;
+  font-weight: 300;
+  font-weight: bold;
+  font-family: Verdana, Geneva, Tahoma, sans-serif;
+  color: var(--color-heading);
+}
+
+.create-user-button {
+  padding: 8px 12px;
+  background-color: #202020;
+  margin-top: 32px;
+  box-shadow: 2px 4px 8px 2px rgba(0, 0, 0, 0.64);
+  color: #fff;
+  border: none;
+  border-radius: 8px;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+  font-size: 200;
+}
+</style>
---
+++ src/views/UserView.vue
@@ -0,0 +1,63 @@
+<script setup lang="ts">
+import router from '@/router/index'
+import { useRoute } from 'vue-router'
+
+const { params } = useRoute()
+const { id } = params
+
+function onCreateUser() {
+  router.push(`/users/${id}/create`)
+}
+</script>
+
+<template>
+  <main>
+    <h1>Olá {{ id }}!</h1>
+    <h1>Oque deseja fazer?</h1>
+
+    <button class="create-user-button" @click="onCreateUser">Criar usuário</button>
+    <div class="child">
+      <router-view></router-view>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+main {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+}
+
+.child {
+  height: 100%;
+  top: 0;
+  left: 0;
+  position: fixed;
+  width: auto;
+}
+
+h1 {
+  width: 100%;
+  text-align: center;
+  font-size: 60px;
+  font-weight: 700;
+  color: hsla(160, 100%, 37%, 1);
+}
+
+.create-user-button {
+  margin-top: 36px;
+  padding: 8px 12px;
+  background-color: #202020;
+  box-shadow: 0px 2px 8px 2px rgba(0, 0, 0, 0.64);
+  color: #fff;
+  border: none;
+  border-radius: 8px;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+  font-size: 200;
+}
+</style>
